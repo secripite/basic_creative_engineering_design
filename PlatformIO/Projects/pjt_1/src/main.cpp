c9f4a8b7c81b427f7b6475ee281af590c41e90c4
secripite <keonho04@gmail.com>
2023-10-06 17:09:58 +0900

    signal light

--- PlatformIO/Projects/pjt_1/src/main.cpp
+++ PlatformIO/Projects/pjt_1/src/main.cpp
@@ -1,12 +1,23 @@
 #include <Arduino.h>
 
+#define red 3
+#define yellow 1
+#define green 2
+
 void setup() {
-  pinMode(0, OUTPUT);
+  pinMode(red, OUTPUT);
+  pinMode(yellow, OUTPUT);
+  pinMode(green, OUTPUT);
 }
 
 void loop() {
-  digitalWrite(0, HIGH);
+  digitalWrite(green, HIGH);
+  delay(2000);
+  digitalWrite(green, LOW);
+  digitalWrite(yellow, HIGH);
   delay(1000);
-  digitalWrite(0, LOW);
-  delay(1000);
+  digitalWrite(yellow, LOW);
+  digitalWrite(red, HIGH);
+  delay(2000);
+  digitalWrite(red, LOW);
 }
